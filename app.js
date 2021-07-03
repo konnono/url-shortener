ea18c1b732545fff5289a1461b538ea65d71fe9d
konnono <konnono@gmail.com>
2021-07-03 16:49:05 +0800

    Feat: Add generateShortURL helper function

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+const generateShortURL = require('./helpers/generateShortURL')
 require('./config/mongoose')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const url = generateShortURL()
+  res.send(generateShortURL())
 })
 
 app.listen(port, () => {
