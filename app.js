8a11ddc2f63d92cf014456bef2a602c1804141fd
konnono <konnono@gmail.com>
2021-07-04 12:39:24 +0800

    Feat: Add display shortUrl result page

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('./node_modules/body-parser')
 
 const app = express()
 const port = 3000
 
+const generateShortURL = require('./helpers/generateShortURL')
+const ShortURL = require('./models/shortURL')
+require('./config/mongoose')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
-const generateShortURL = require('./helpers/generateShortURL')
-require('./config/mongoose')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/', (req, res) => {
-  const url = generateShortURL()
-  res.send(generateShortURL())
+  // console.log(req.body.url)
+  const short = generateShortURL()
+  const shortUrl = req.get('host') + '/' + short
+  console.log(shortUrl)
+  ShortURL.create({
+    original: req.body.url,
+    short: short
+  })
+  res.render('shorturls', { shortUrl })
 })
 
 app.listen(port, () => {
