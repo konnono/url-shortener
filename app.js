f06116251591e31129ea50dfcd31a820aa21b62b
konnono <konnono@gmail.com>
2021-07-10 16:06:33 +0800

    Feat: Prepare for Heroku deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('./node_modules/body-parser')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const generateShortURL = require('./helpers/generateShortURL')
 const ShortURL = require('./models/shortURL')
